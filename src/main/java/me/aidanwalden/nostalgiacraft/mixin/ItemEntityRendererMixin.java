e9e85ede5e36ba7cdef750de29d462551f0992ca
Aidan <aidan.walden15@gmail.com>
2021-01-17 20:39:33 -0500

    1.16.5

--- src/main/java/me/aidanwalden/nostalgiacraft/mixin/ItemEntityRendererMixin.java
+++ src/main/java/me/aidanwalden/nostalgiacraft/mixin/ItemEntityRendererMixin.java
@@ -3,15 +3,24 @@ package me.aidanwalden.nostalgiacraft.mixin;
 import me.aidanwalden.nostalgiacraft.NostalgiacraftCore;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.ItemEntityRenderer;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.FlyingItemEntity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -26,10 +35,22 @@ public abstract class ItemEntityRendererMixin extends EntityRenderer<ItemEntity>
         super(dispatcher);
     }
 
+    private ItemEntity entity;
+
     @Inject(method = "render", at = @At(value = "HEAD"))
     public void render(ItemEntity itemEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        entity = itemEntity;
         if(NostalgiacraftCore.doOldCactusRender && itemEntity.getStack().getItem() == Items.CACTUS) {
             matrixStack.scale(2, 2, 2);
         }
+        else if(!(itemEntity.getStack().getItem() instanceof BlockItem)) {
+
+        }
+    }
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderer;renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V"))
+    public void renderItem(ItemRenderer itemRenderer, ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model)
+    {
+        itemRenderer.renderItem(stack, ModelTransformation.Mode.GROUND, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers);;
     }
 }
